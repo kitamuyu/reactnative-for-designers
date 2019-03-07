5181d1188d14bf36e475fc46b966797394a72add
Yu Kitamura <kitamura.ky.monkey@gmail.com>
2019-03-07 23:34:21 +0900

    Apply redux to avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import { connect } from "react-redux";
+
+function mapStateToProps(state) {
+  return {
+    name: state.name
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    updateName: name =>
+      dispatch({
+        type: "UPDATE_NAME",
+        name: name
+      })
+  };
+}
 
 class Avatar extends React.Component {
   state = {
@@ -7,14 +24,20 @@ class Avatar extends React.Component {
   };
 
   componentDidMount() {
-    fetch("https://uinames.com/api/?ext")
+    fetch("https://uifaces.co/api?limit=1&random", {
+      headers: new Headers({
+        "X-API-KEY": "eeaafbe81657073cd70ac6e3de1bd6"
+      })
+    })
       .then(response => response.json())
       .then(response => {
         console.log(response);
 
         this.setState({
-          photo: response.photo
+          photo: response[0].photo
         });
+
+        this.props.updateName(response[0].name);
       });
   }
 
@@ -23,7 +46,10 @@ class Avatar extends React.Component {
   }
 }
 
-export default Avatar;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Avatar);
 
 const Image = styled.Image`
   width: 44px;
