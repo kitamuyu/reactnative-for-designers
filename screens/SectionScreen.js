13dc17cdf8c75307b68d50ba9de4690b833375c0
Yu Kitamura <kitamura.ky.monkey@gmail.com>
2019-03-11 16:18:58 +0900

    Use MarkDown

--- screens/SectionScreen.js
+++ screens/SectionScreen.js
@@ -1,7 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import { TouchableOpacity, StatusBar, WebView, Linking } from "react-native";
+import {
+  TouchableOpacity,
+  StatusBar,
+  WebView,
+  Linking,
+  ScrollView
+} from "react-native";
 import { Icon } from "expo";
+import Markdown from "react-native-showdown";
 
 class SectionScreen extends React.Component {
   static navigationOptions = {
@@ -21,34 +28,35 @@ class SectionScreen extends React.Component {
     const section = navigation.getParam("section");
 
     return (
-      <Container>
-        <StatusBar hidden />
-        <Cover>
-          <Image source={section.image} />
-          <Wrapper>
-            <Logo source={section.logo} />
-            <Subtitle>{section.subtitle}</Subtitle>
-          </Wrapper>
-          <Title>{section.title}</Title>
-          <Caption>{section.caption}</Caption>
-        </Cover>
-        <TouchableOpacity
-          onPress={() => {
-            this.props.navigation.goBack();
-          }}
-          style={{ position: "absolute", top: 20, right: 20 }}
-        >
-          <CloseView>
-            <Icon.Ionicons
-              name="ios-close"
-              size={36}
-              color="#4775f2"
-              style={{ marginTop: -2 }}
-            />
-          </CloseView>
-        </TouchableOpacity>
-        <Content>
-          <WebView
+      <ScrollView>
+        <Container>
+          <StatusBar hidden />
+          <Cover>
+            <Image source={section.image} />
+            <Wrapper>
+              <Logo source={section.logo} />
+              <Subtitle>{section.subtitle}</Subtitle>
+            </Wrapper>
+            <Title>{section.title}</Title>
+            <Caption>{section.caption}</Caption>
+          </Cover>
+          <TouchableOpacity
+            onPress={() => {
+              this.props.navigation.goBack();
+            }}
+            style={{ position: "absolute", top: 20, right: 20 }}
+          >
+            <CloseView>
+              <Icon.Ionicons
+                name="ios-close"
+                size={36}
+                color="#4775f2"
+                style={{ marginTop: -2 }}
+              />
+            </CloseView>
+          </TouchableOpacity>
+          <Content>
+            {/* <WebView
             source={{ html: section.content + htmlStyles }}
             scalesPageToFit={false}
             scrollEnabled={false}
@@ -61,9 +69,16 @@ class SectionScreen extends React.Component {
                 Linking.openURL(event.url);
               }
             }}
-          />
-        </Content>
-      </Container>
+          /> */}
+            <Markdown
+              body={section.content}
+              pureCSS={htmlStyles}
+              scalesPageToFit={false}
+              scrollEnabled={false}
+            />
+          </Content>
+        </Container>
+      </ScrollView>
     );
   }
 }
@@ -77,17 +92,60 @@ const htmlContent = `
 `;
 
 const htmlStyles = `
-  <style>
     * {
       font-family: -apple-system; 
       margin: 0;
       padding: 0;
+      font-size: 17px; 
+      font-weight: normal; 
+      color: #3c4560;
+      line-height: 24px;
     }
-  </style>
+    h2 {
+      font-size: 20px;
+      text-transform: uppercase;
+      color: #b8bece;
+      font-weight: 600;
+      margin-top: 50px;
+    }
+  
+    p {
+      margin-top: 20px;
+    }
+  
+    a {
+      color: #4775f2;
+      font-weight: 600;
+      text-decoration: none;
+    }
+  
+    strong {
+      font-weight: 700;
+    }
+
+    img {
+      width: 100%;
+      border-radius: 10px;
+      margin-top: 20px; 
+    }
+
+    pre {
+      padding: 20px;
+      background: #212C4F;
+      overflow: hidden;
+      word-wrap: break-word;
+      border-radius: 10px;
+        margin-top: 20px;
+    }
+    
+    code {
+      color: white;
+    }
 `;
 
 const Content = styled.View`
-  height: 100%;
+  height: 1000px;
+  padding: 20px;
 `;
 
 const Container = styled.View`
